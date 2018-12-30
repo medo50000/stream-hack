483985fffda356f2549dedace7af7555943cf160
medo50000 <45098450+medo50000@users.noreply.github.com>
2018-12-30 13:01:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', message => {
   if (!message.content.startsWith(PrEfix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== 'هنا ام اي دي شخص') return;
+  if (message.author.id !== '523116488861810689') return;
 if (message.content.startsWith(PrEfix + 'wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.sendMessage(`**✅  : ${argresult}**`)

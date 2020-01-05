86cb97e1c940c8d6572ee72fa22cf6db8cc50357
medo50000 <45098450+medo50000@users.noreply.github.com>
2020-01-05 20:12:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', message => {
   if (!message.content.startsWith(PrEfix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== '402251000167006209') return;
+  if (message.author.id !== '468479699543130132') return;
 if (message.content.startsWith(PrEfix + 'wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.sendMessage(`**✅  : ${argresult}**`)
@@ -32,7 +32,7 @@ if (message.content.startsWith(PrEfix + 'st')) {
 
 client.on('message', message => {
 if(message.content.startsWith('c')) {
-if(message.author.id !== '517409391570583574') return;
+if(message.author.id !== '468479699543130132') return;
 var args = message.content.split(' ').slice(1).join(' ');
 message.channel.send(args);
 }
